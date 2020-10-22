51f61c51973db308a56f17c9b7f4bb9ef56c9cb5
GusOs <gus_92@hotmail.es>
2020-10-22 17:10:47 +0200

    Add forest enemy script

---
+++ Fantasy/Assets/Scripts/ForestEnemyManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ForestEnemyManager : MonoBehaviour
+{
+    //Transform del enemigo
+    public Transform player;
+
+    //Distancia de detección
+    public float distance = 25f;
+
+    //Animator del enemigo
+    public Animator anim;
+
+    UnityEngine.AI.NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    /*Si la posición del jugador respecto al enemigo es menor que la distancia
+     * Siempre busca al jugador
+     * si la dirección es mayor de 5, cambia de animación
+    */
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < distance)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("idle", true);
+
+            if (direction.magnitude < 12)
+            {
+                anim.SetBool("idle", false);
+                anim.SetBool("attack", false);
+                anim.SetInteger("moving", 3);
+                nav = GetComponent<NavMeshAgent>();
+                nav.SetDestination(player.position);
+            }
+            if (direction.magnitude < 2)
+            {
+                anim.SetInteger("moving", 0);
+                anim.SetBool("attack", true);
+            }
+        }
+    }
+}
