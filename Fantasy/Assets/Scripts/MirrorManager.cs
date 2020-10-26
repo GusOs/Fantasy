9d84504abe7406ddae12ad1d4051bddbaa8141aa
GusOs <gus_92@hotmail.es>
2020-10-26 14:58:20 +0100

    Update scene temple jungle

---
+++ Fantasy/Assets/Scripts/MirrorManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MirrorManager : MonoBehaviour
+{
+
+    private Collider playerCollision;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider playerCollision)
+    {
+        if(playerCollision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Temple_Jungle");
+        }
+    }
+}
