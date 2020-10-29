c6cb040dcf9b3a33605e0ca453d57f350d724ee7
GusOs <gus_92@hotmail.es>
2020-10-29 16:02:44 +0100

    Add script item life

---
+++ Fantasy/Assets/Scripts/ItemLife.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemLife : MonoBehaviour
+{
+    //Colision
+    private Collider itemCollision;
+
+    //Efecto al colisionar
+    public GameObject lifeEffect;
+
+    //Audio al colisionar
+    public Sound life;
+
+    //Referencia a la vida del jugador
+    public GameObject lifePlayer;
+
+    //Referencia al script del jugador
+    private PlayerMovement playerScript;
+
+    void Start()
+    {
+        itemCollision = GetComponent<Collider>();
+    }
+
+    /*Comprobar si ha colisionado con el jugador
+     * Reproducir audio de vida
+     * añadir 25 de vida al jugador
+     * Instanciar el efecto de vida
+     * Spawner más items
+     * desactivar el item
+    */
+    private void OnTriggerEnter(Collider itemCollision)
+    {
+        if (itemCollision.CompareTag("Player") && playerScript.lifePlayer < 100)
+        {
+            //AudioManager.Instance.PlaySound(life);
+            (itemCollision.gameObject.GetComponent("PlayerMovement") as PlayerMovement).lifePlayer += 20;
+            //Debug.Log((itemCollision.gameObject.GetComponent("PlayerMovement") as PlayerMovement).lifePlayer);
+            //Instantiate(lifeEffect, this.transform.position, Quaternion.LookRotation(this.transform.position));
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Fantasy/Assets/Scripts/Sword.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+
+    private Collider enemyCollision;
+
+    public Sound Hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCollision = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider enemyCollision)
+    {
+        if(enemyCollision.CompareTag("Enemy"))
+        {
+            AudioManager.Instance.PlaySound(Hit);
+        }
+    }
+
+}
