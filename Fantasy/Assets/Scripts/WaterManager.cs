a972e5ad082096b30068d37a69a0a21471eb612f
GusOs <gus_92@hotmail.es>
2020-11-05 14:33:43 +0100

    Add script water

---
+++ Fantasy/Assets/Scripts/WaterManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterManager : MonoBehaviour
+{
+
+    private Collider water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        water = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider water)
+    {
+        if (water.gameObject.CompareTag("Player"))
+        {
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
