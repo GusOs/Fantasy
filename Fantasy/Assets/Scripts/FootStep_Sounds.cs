a548f72bfc4969f1549cd57a7c5504ff1bba96c7
GusOs <gus_92@hotmail.es>
2020-11-06 15:39:24 +0100

    Add footsteps sounds player

---
+++ Fantasy/Assets/Scripts/FootStep_Sounds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStep_Sounds : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Animator anim;
+    public float stepDelay;
+    public AudioClip defaultClip;
+    private bool couroutineOn;
+
+    void Start()
+    {
+
+        anim = this.gameObject.GetComponent<Animator>();
+
+        couroutineOn = true;
+        audioSource.clip = defaultClip;
+
+        StartCoroutine(Walking());
+    }
+
+    IEnumerator Walking()
+    {
+
+        while (couroutineOn == true)
+        {
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run") || anim.GetCurrentAnimatorStateInfo(0).IsName("Walk") || anim.GetCurrentAnimatorStateInfo(0).IsName("Walk_back"))
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                audioSource.Stop();
+            }
+
+            yield return new WaitForSeconds(stepDelay);
+
+        }
+    }
+}
