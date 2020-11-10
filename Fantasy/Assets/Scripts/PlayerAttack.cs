2f6034b270b16d5a60a682210127fb67affd9c16
GusOs <gus_92@hotmail.es>
2020-11-10 15:11:08 +0100

    Add player attack to boss

---
+++ Fantasy/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    //Daño del ataque
+    public float attackSword = 5.0f;
+
+    public Sound Hit;
+
+    private Collider swordCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider swordCollider)
+    {
+        if (swordCollider.gameObject.CompareTag("Boss"))
+        {
+            (swordCollider.gameObject.GetComponent("CentaurManager") as CentaurManager).lifeBoss -= attackSword;
+            GameManager.Instance.GameOver();
+        }
+    }
+}
