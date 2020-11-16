5c28a337ee50f25905486653b2020141583aa79d
GusOs <gus_92@hotmail.es>
2020-11-17 00:11:47 +0100

    Change healthBar

--- Fantasy/Assets/Scripts/HealthBar.cs
+++ Fantasy/Assets/Scripts/HealthBar.cs
@@ -7,7 +7,7 @@ public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
-    public Gradient gradient;
+    //public Gradient gradient;
     public Image fill;
 
 
@@ -16,13 +16,13 @@ public class HealthBar : MonoBehaviour
         slider.maxValue = health;
         slider.value = health;
 
-        fill.color = gradient.Evaluate(1f);
+        //fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health)
     {
         slider.value = health;
 
-        fill.color = gradient.Evaluate(slider.normalizedValue);
+        //fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
