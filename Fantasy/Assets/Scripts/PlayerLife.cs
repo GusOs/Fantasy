cca71dc770871bb2105d74fba1d45db946c9acf5
GusOs <gus_92@hotmail.es>
2020-11-17 00:06:40 +0100

    Update lifeBar player

--- Fantasy/Assets/Scripts/PlayerLife.cs
+++ Fantasy/Assets/Scripts/PlayerLife.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class PlayerLife : MonoBehaviour
 
     public Sound death;
 
+    public Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +26,7 @@ public class PlayerLife : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(20);
-        }
+        slider.value = currentHealth;
     }
 
     void TakeDamage(int damage)
